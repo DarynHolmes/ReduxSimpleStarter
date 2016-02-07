7bc73f1969e60d334363f62191e1f43573bf51d8
DarynHolmes <DarynHolmes@gmail.com>
2016-02-07 20:17:22 +0000

    adding google maps

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,7 +1,6 @@
 import { FETCH_WEATHER } from '../actions/index';
 
 export default function(state = [], action) {
-  console.log('action', FETCH_WEATHER);
   switch (action.type) {
     case FETCH_WEATHER:
       const data = [ action.payload.data, ...state ]; // same as [action.payload.data].concat(sate); -> new item at the top 
